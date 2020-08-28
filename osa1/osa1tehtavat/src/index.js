89ba9ec394f903dcb4e2f6b4c8500653cfbcbb66
Amir Bardo <xequitep@gmail.com>
2020-08-29 01:25:45 +0300

    1.8 tehty

--- osa1/osa1tehtavat/src/index.js
+++ osa1/osa1tehtavat/src/index.js
@@ -4,6 +4,21 @@ import ReactDOM from 'react-dom';
 const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 );
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <h2>Statistics</h2>
+      Good: <Display value={props.good} />
+      Neutral: <Display value={props.neutral} />
+      Bad: <Display value={props.bad} />
+      All: <Display value={props.all} />
+      Average: <Display value={(props.good + props.bad * -1) / props.all} />
+      Positive (%): <Display value={(props.good / props.all) * 100} />
+    </div>
+  );
+};
+
 const Display = (props) => <div>{props.value}</div>;
 
 const App = (props) => {
@@ -11,28 +26,27 @@ const App = (props) => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  let All = good + neutral + bad;
+  const [all, setAll] = useState(0);
 
   return (
     <React.Fragment>
       <div>
         <h2>Give feedback</h2>
-        <Button handleClick={() => setGood(good + 1)} text="Good" />
-        <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
-        <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+        <Button
+          handleClick={() => setGood(good + 1) & setAll(all + 1)}
+          text="Good"
+        />
+        <Button
+          handleClick={() => setNeutral(neutral + 1) & setAll(all + 1)}
+          text="Neutral"
+        />
+        <Button
+          handleClick={() => setBad(bad + 1) & setAll(all + 1)}
+          text="Bad"
+        />
       </div>
-      <div>
-        <h2>Statistics</h2>
-        Good: <Display value={good} />
-        Neutral: <Display value={neutral} />
-        Bad: <Display value={bad} />
-        All: <Display value={All} />
-        Average:
-        <Display value={(good + bad * -1) / All} />
-        Positive (%): <Display value={(good / All) * 100} />
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
     </React.Fragment>
   );
 };
-
 ReactDOM.render(<App />, document.getElementById('root'));
