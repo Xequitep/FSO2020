7005424cf031028304a510a295463271e2c06450
Amir Bardo <xequitep@gmail.com>
2020-09-09 00:04:49 +0300

    react osa1 hienosaadetty kuntoon

--- osa1/osa1tehtavat2/src/index.js
+++ osa1/osa1tehtavat2/src/index.js
@@ -62,4 +62,4 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
 ];
 
-ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
